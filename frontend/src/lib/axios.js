b754dbb94dafdfcd0217bbf5a91a12648fcae2ee
manishgoudkatta <manishgoud796@gmail.com>
2026-01-26 22:00:46 +0530

    3nd commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,19 +1,22 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.MODE === "development" ?  : "/api",
-//   withCredentials: true,
-// });
-
-
 import axios from "axios";
-
-
 
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? import.meta.env.VITE_BASE_URL + "/api"
-      : "https://real-time-chatapp-bymanish-1.onrender.com/api",
+      ? "http://localhost:3000/api"
+      : " https://real-time-chatapp-bymanish-2.onrender.com",
   withCredentials: true,
 });
+
+
+// import axios from "axios";
+
+
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? import.meta.env.VITE_BASE_URL + "/api"
+//       : "https://real-time-chatapp-bymanish-1.onrender.com/api",
+//   withCredentials: true,
+// });
